e209c279cc9881875fefb7752f30f5e60edbeac1
lucky845 <lc1447545564@gmail.com>
2022-03-16 19:08:06 +0800

    预约下单功能

--- api/yygh/orderinfo.js
+++ api/yygh/orderinfo.js
@@ -3,6 +3,7 @@ import request from '@/utils/request'
 const API = `/api/order/orderInfo`
 
 export default {
+  // 提交预约,生成订单
  submitOrder(scheduleId, patientId) {
   return request({
     url: `${API}/auth/submitOrder/${scheduleId}/${patientId}`,
